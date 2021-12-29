76d654e17cd960a6d541aa9f8f233abd7d587f90
Oliver Hughes <p2421386@my365.dmu.ac.uk>
2021-12-29 19:32:12 +0000

    new front page layout

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,3 @@
-"use strict";
 
-let intro = "Hey, I'm Ollie Hughes :)"
-let counter = 0
-
-
-function typeWriter() {
-  if(counter < intro.length) {
-    document.getElementById("typing-text").innerHTML += intro.charAt(counter);
-    counter++
-    setTimeout(typeWriter, 50);
-  } else {
-    
-  }
-}
-
-menuToggler.addEventListener('click', event => {
-  menu.classList.toggle('menu--open');
-});
-
-
-window.addEventListener('load', () => {
-  typeWriter();
-})
 
 
